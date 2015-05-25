ab2686cb8a53b82606cfd86ee5e80c319f5b048a
uenpbenchmark <uenpbenchmark@gmail.com>
2015-05-25 03:42:25 -0300

    25 de maio

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import webapp2
 import os
 import time
 from google.appengine.ext import ndb
+from google.appengine.ext.webapp import template
 
 class User(ndb.Model):
 	name = ndb.StringProperty()
@@ -19,53 +20,49 @@ class Comment(ndb.Model):
 class MainHandler(webapp2.RequestHandler):
 
 	def post(self):
-		#carrega as variaveis enviadas por POST
-		
-		operationcount = int(self.request.get("operationcount"))
-		workload = self.request.get("workload")
-		
-		#inicializa as variaveis reads e writes
-		
-		reads = 0
-		writes = 0
-		
-		#atribui o valor para elas de acordo com o workload, 
-		#por exemplo: para 500 operacoes e workload B(50/50), o resultado é 250 escritas e 250 leituras
-		
-		if(workload == "a"):
-			reads = 0.5 * operationcount
-			writes = 0.5 * operationcount
-		elif(workload == "b"): #elif significa else if
-			reads = 0.95 * operationcount
-			writes = 0.5 * operationcount
-		elif(workload == "c"):
-			reads = operationcount
-			writes = 0
-		elif(workload == "w"):
-			reads = 0
-			writes = operationcount
+		#1. Carrega variáveis necessárias para o workload por post
+			workload = self.request.get("workload")
+			operationcount = self.request.get("operationcount")
+			schema = self.request.get("schema")
+			writes = self.request.get("writes")
+			reads = self.request.get("reads")
 			
-		#começa a contar o tempo, gravando a hora atual em milisegundos
-		t0 = time.clock() 
-		
-		#for readOperations  in readOperationsRange(1, reads):
-		#executa leituras aqui
-		
-		#for writeOperations  in writeOperationsRange(1, writes):
-		#executa escritas aqui
-		
-		#pára de contar o tempo, subtraindo a hora atual da hora inicial
-		total = time.clock() - t0
-		self.response.write("Terminado, duracao: " + str(total))
-
-# Workload A: Update heavy workload
-# This workload has a mix of 50/50 reads and writes.
-# Workload B: Read mostly workload
-# This workload has a 95% reads and 5% write mix
-# Workload C: Read only
-# This workload is 100% read
-# Workload W: Write only
-# This workload is 100% write
+			#2. Armazena o tempo inicial
+			initialTime = time.clock() 
+			
+			#3. Realiza as operações de acordo com o esquema recebido por post
+			# No Python, cria-se grupos de entidade da seguinte forma: 
+			# Crie um objeto pai; persista o objeto pai com a funcao put(), que retorna uma chave;
+			# Crie um objeto filho e passe a chave do objeto pai como parâmetro (parent = chaveDoPai)
+			if(schema == "1"):
+				
+				user = User()
+				userKey = user.put()
+				article = Article(parent = userKey)
+				articleKey = article.put()
+			
+				for writeOperations  in range(0, int(writes)):
+					comment = Comment(parent = articleKey)
+					comment.put()
+				for readOperations  in range(0, int(reads)):
+					query = Comment.query().get()
+			
+			elif(schema == "2"):
+				#No esquema 2, o número de escritas é dividido por 2 porque a cada iteração, são inseridas 3 entidades
+				for writeOperations  in range(0, int(writes)/3):
+					user = User()
+					userKey = user.put()
+					article = Article(parent = userKey)
+					article.put()
+					comment = Comment(parent = userKey)
+					comment.put()
+				for readOperations  in range(0, int(reads)):
+					query = Comment.query().get()
+			
+			totalTime = time.clock() - initialTime
+			
+			dictionary = {'expectedReads:': reads, 'reads': "a implementar", 'expectedWrites': writes, 'writes': "a implementar" ,'totalTime': totalTime }
+			self.response.write(template.render('results.html', dictionary))
 
 app = webapp2.WSGIApplication([
     ('/', MainHandler)
